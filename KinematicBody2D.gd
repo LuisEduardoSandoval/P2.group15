e38777603eba526d91ae317e6a4ae3ae2088422f
LuisEduardoSandoval <55498145+LuisEduardoSandoval@users.noreply.github.com>
2019-11-23 16:29:47 -0600

    changes to movement testing

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -4,26 +4,31 @@ export (int) var speed = 200
 
 var velocity = Vector2()
 
+var Mouse_Position
 
 
 func get_input():
-    look_at(get_global_mouse_position())
     velocity = Vector2()
+    if Input.is_action_pressed('right'):
+        velocity.x += 1
+    if Input.is_action_pressed('left'):
+        velocity.x -= 1
     if Input.is_action_pressed('down'):
-        velocity = Vector2(-speed, 0).rotated(rotation)
+        velocity.y += 1
     if Input.is_action_pressed('up'):
-        velocity = Vector2(speed, 0).rotated(rotation)
-
-
-
-
+        velocity.y -= 1
+    velocity = velocity.normalized() * speed
 
 
 func _physics_process(delta):
     get_input()
     velocity = move_and_slide(velocity)
 
-
+func _process(delta):
+	Mouse_Position = get_local_mouse_position()*0.1
+	#rotation += Mouse_Position.angle()
+	get_node("root/Hip/Torso/Head").rotation += Mouse_Position.angle()
+	pass
 
 
 
@@ -31,6 +36,8 @@ func _physics_process(delta):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("root/AnimationPlayer").play("idle")
+	
+
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
