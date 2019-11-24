9286ef789509497838dd77263d394a397ab195e0
LuisEduardoSandoval <55498145+LuisEduardoSandoval@users.noreply.github.com>
2019-11-23 23:03:23 -0600

    modified player movement

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,7 +3,11 @@ extends KinematicBody2D
 export (int) var speed = 200 #character move speed
 var velocity = Vector2() #character velocity in 2 Dimensions
 var Mouse_Position 
-var lastXpos #last Direction on axis so head movement is correct 
+var lastXpos = false #last Direction on axis so head movement is correct false means looking right
+
+
+
+
 
 
 
@@ -32,24 +36,34 @@ func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
 
-func _process(delta):
-	Mouse_Position = get_local_mouse_position() #this must be initialized or it will crash when it calls
+func _process(delta): 
+	Mouse_Position = get_local_mouse_position() #this must be initialized or it will crash when it calls 
+	
+
+	
+	
+	
 	if Input.is_action_pressed('right'): #had to check movement here as it controls rotational diretion of head
-		Mouse_Position = get_local_mouse_position()*0.1
-	if Input.is_action_pressed('left'):
-		Mouse_Position.x = get_local_mouse_position().x*-0.1
-		Mouse_Position.y = get_local_mouse_position().y*0.1
+		Mouse_Position = get_local_mouse_position() #if movement in the right direction updates head directional position
+	if Input.is_action_pressed('left'): 
+		Mouse_Position.x = -get_local_mouse_position().x #changes head x to match movements
+		Mouse_Position.y = get_local_mouse_position().y
 	if lastXpos == true:
-		Mouse_Position.x = get_local_mouse_position().x*-0.1
-		Mouse_Position.y = get_local_mouse_position().y*0.1
+		Mouse_Position.x = -get_local_mouse_position().x
+		Mouse_Position.y = get_local_mouse_position().y
+	if lastXpos == false:
+		Mouse_Position.x = get_local_mouse_position().x
+		Mouse_Position.y = get_local_mouse_position().y
 	get_node("root/Hip/Torso/Head").rotation += Mouse_Position.angle() #changes the angle of the head to match mouse
-#	if lastXpos == false: #IN_PROGRESS CHANGES WHEN MOUSE X IS TOO FAR FOR HEAD TO FOLLOW POSITION CHANGES
-#		if get_local_mouse_position().x < $root.global_position.x -200:
-#			get_node("root").set_scale(Vector2(-1,-1))
-#		if get_local_mouse_position().x > $root.global_position.x -200:
-#			get_node("root").set_scale(Vector2(1,1))
-#			Mouse_Position.x = get_local_mouse_position().x*0.1
-	#if get_local_mouse_position().x < $root/Hip.global_position.x :
+	if lastXpos == false: #if player is facing right!
+		if get_local_mouse_position().x < $root.position.x : # if players mouse is on or behind root node
+			get_node("root").set_scale(Vector2(-1,1)) #models is scaled verticaly and horizontally
+			lastXpos = true
+	if lastXpos == true: 
+		if get_local_mouse_position().x > $root.position.x :
+			get_node("root").set_scale(Vector2(1,1))
+			lastXpos = false
+
 	pass
 	
 
@@ -61,3 +75,4 @@ func _ready():
 	get_node("root/AnimationPlayer").play("idle")
 	pass
 
+	
