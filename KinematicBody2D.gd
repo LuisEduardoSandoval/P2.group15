4ebdf856b4f401f343304557dd3db5eb2d2ace47
LuisEduardoSandoval <55498145+LuisEduardoSandoval@users.noreply.github.com>
2019-11-23 20:57:44 -0600

    changes to movement for more fluidity, and test bkg

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,45 +1,63 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 200 #character move speed
+var velocity = Vector2() #character velocity in 2 Dimensions
+var Mouse_Position 
+var lastXpos #last Direction on axis so head movement is correct 
 
-var velocity = Vector2()
-
-var Mouse_Position
 
 
 func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed('right'):
-        velocity.x += 1
-    if Input.is_action_pressed('left'):
-        velocity.x -= 1
-    if Input.is_action_pressed('down'):
-        velocity.y += 1
-    if Input.is_action_pressed('up'):
-        velocity.y -= 1
-    velocity = velocity.normalized() * speed
-
+	velocity = Vector2()
+	if Input.is_action_pressed('right'): 
+		velocity.x += 1 
+		get_node("root/AnimationPlayer").play("Move") 
+		get_node("root").set_scale(Vector2(1,1)) #flips horazontally
+		lastXpos = false 
+	if Input.is_action_pressed('left'):
+		velocity.x -= 1
+		get_node("root/AnimationPlayer").play("Move")
+		get_node("root").set_scale(Vector2(-1,1))
+		lastXpos = true
+		
+	if Input.is_action_pressed('down'):
+		velocity.y += 1
+	if Input.is_action_pressed('up'):
+		velocity.y -= 1
+	if (velocity.x == 0):
+		get_node("root/AnimationPlayer").play("idle")
+	velocity = velocity.normalized()* speed
 
 func _physics_process(delta):
-    get_input()
-    velocity = move_and_slide(velocity)
+	get_input()
+	velocity = move_and_slide(velocity)
 
 func _process(delta):
-	Mouse_Position = get_local_mouse_position()*0.1
-	#rotation += Mouse_Position.angle()
-	get_node("root/Hip/Torso/Head").rotation += Mouse_Position.angle()
+	Mouse_Position = get_local_mouse_position() #this must be initialized or it will crash when it calls
+	if Input.is_action_pressed('right'): #had to check movement here as it controls rotational diretion of head
+		Mouse_Position = get_local_mouse_position()*0.1
+	if Input.is_action_pressed('left'):
+		Mouse_Position.x = get_local_mouse_position().x*-0.1
+		Mouse_Position.y = get_local_mouse_position().y*0.1
+	if lastXpos == true:
+		Mouse_Position.x = get_local_mouse_position().x*-0.1
+		Mouse_Position.y = get_local_mouse_position().y*0.1
+	get_node("root/Hip/Torso/Head").rotation += Mouse_Position.angle() #changes the angle of the head to match mouse
+#	if lastXpos == false: #IN_PROGRESS CHANGES WHEN MOUSE X IS TOO FAR FOR HEAD TO FOLLOW POSITION CHANGES
+#		if get_local_mouse_position().x < $root.global_position.x -200:
+#			get_node("root").set_scale(Vector2(-1,-1))
+#		if get_local_mouse_position().x > $root.global_position.x -200:
+#			get_node("root").set_scale(Vector2(1,1))
+#			Mouse_Position.x = get_local_mouse_position().x*0.1
+	#if get_local_mouse_position().x < $root/Hip.global_position.x :
 	pass
-
-
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	get_node("root/AnimationPlayer").play("idle")
 	
 
+
+
+
+
+func _ready():
+	get_node("root/AnimationPlayer").play("idle")
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
