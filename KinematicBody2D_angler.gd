9b927e5237a4b82ec663cc0c8bbe3ff90c452550
LuisEduardoSandoval <luis.e.sandoval@ttu.edu>
2019-11-27 20:57:51 -0700

    added paralax bkg, modified level, optimized fps a bit

--- KinematicBody2D_angler.gd
+++ KinematicBody2D_angler.gd
@@ -1,12 +1,17 @@
 extends KinematicBody2D
-
+export var speed = Vector2(200,200) #character move speed
+var velocity: = Vector2.ZERO
+export var gravity: = 30.0
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+func _physics_process(delta: float) -> void:
+	velocity.x = -0.05
+	move_and_slide(velocity)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Node2D/AnimationPlayer.play("Move")
+	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Level_TEST_CUSTOM.gd
+++ Level_TEST_CUSTOM.gd
@@ -12,3 +12,11 @@ func _ready():
 #func _process(delta):
 #	pass
 
+const TIMER_LIMIT = 2.0
+var timer = 0.0
+
+func _process(delta):
+	timer += delta
+	if timer > TIMER_LIMIT:
+		timer = 0.0
+		print("fps: " + str(Engine.get_frames_per_second()))
