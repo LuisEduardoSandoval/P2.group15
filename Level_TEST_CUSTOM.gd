f33f8bc28516d25783f23dd459b3f3052a888441
Caleb Horn <calebahorn@gmail.com>
2019-12-01 17:50:53 -0600

    reset

--- Level_TEST_CUSTOM.gd
+++ Level_TEST_CUSTOM.gd
@@ -11,25 +11,18 @@ onready var Tuna_Position = $Tuna_Kinematic.position.x
 #func _process(delta):
 #	pass
 
-const TIMER_LIMIT = 4.0
+const TIMER_LIMIT = 2.0
 var timer = 0.0
-var finish = false 
+
 func _process(delta):
 	timer += delta
 	if timer > TIMER_LIMIT:
 		timer = 0.0
 		print("fps: " + str(Engine.get_frames_per_second())) #prints something every 2 seconds calling timer limit
-		if finish == true:
-			get_tree().change_scene("TlteScreen.tscn")
 
-		if Tuna_Position -4000 > $Tuna_Kinematic.position.x : #little fish poopy movement
-			$Tuna_Kinematic.position.x += 4000
-			$School_Fish_Kinematic.position.x += 4000
-	if $KinematicBody2D.position.y < 340:
-		$Win/Win.set_visible(true)
-		finish = true
-		
-		
+		if Tuna_Position -1742 > $Tuna_Kinematic.position.x : #little fish poopy movement
+			$Tuna_Kinematic.position.x += 1742
+			$School_Fish_Kinematic.position.x += 1742
 
 
 func _ready():	
