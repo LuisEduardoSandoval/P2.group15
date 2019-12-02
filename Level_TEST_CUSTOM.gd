3a62297e0074af6f3afb166eb8300712a52612f8
LuisEduardoSandoval <55498145+LuisEduardoSandoval@users.noreply.github.com>
2019-12-01 17:02:06 -0700

    Add files via upload

---
+++ Level_TEST_CUSTOM.gd
@@ -0,0 +1,41 @@
+extends Node
+
+
+onready var Tuna_Position = $Tuna_Kinematic.position.x
+
+
+# Called when the node enters the scene tree for the first time.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+const TIMER_LIMIT = 4.0
+var timer = 0.0
+var finish = false 
+func _process(delta):
+	timer += delta
+	if timer > TIMER_LIMIT:
+		timer = 0.0
+		print("fps: " + str(Engine.get_frames_per_second())) #prints something every 2 seconds calling timer limit
+		if finish == true:
+			get_tree().change_scene("TlteScreen.tscn")
+
+		if Tuna_Position -4000 > $Tuna_Kinematic.position.x : #little fish poopy movement
+			$Tuna_Kinematic.position.x += 4000
+			$School_Fish_Kinematic.position.x += 4000
+	if $KinematicBody2D.position.y < 340:
+		$Win/Win.set_visible(true)
+		finish = true
+		
+		
+
+
+func _ready():	
+	var player = AudioStreamPlayer.new()
+	self.add_child(player) 
+	player.stream = load("res://Sound/sonar-ping.wav")
+	player.set_volume_db(-18)
+	player.play()
+	pass # Replace with function body.
